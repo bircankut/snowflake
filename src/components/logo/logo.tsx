d3000675ffef3c76ba3296a395da2bb77890d99a
bircan <bircankut@gmail.com>
2022-06-29 17:01:36 +0300

    Add logo to header section

--- src/components/logo/logo.tsx
+++ src/components/logo/logo.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React from "react"
+import Image from 'next/image'
 import style from './logo.module.css'
 
 interface LogoProps {}
@@ -6,7 +7,7 @@ interface LogoProps {}
 const Logo = ({}: LogoProps) => {
   return(
     <div className={style.logo}>
-      logo
+      <Image src='/snowflake.svg' height={30} width={30} />
     </div>
   )
 }
