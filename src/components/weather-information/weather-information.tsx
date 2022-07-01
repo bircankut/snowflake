ce551fbcc6e759eb186f6938ae4d90805ec9a8c8
bircan <bircankut@gmail.com>
2022-07-01 14:43:11 +0300

    Add weather icons

--- src/components/weather-information/weather-information.tsx
+++ src/components/weather-information/weather-information.tsx
@@ -1,27 +1,82 @@
 import React from "react"
 import style from './weather-information.module.css'
+import {Data} from "../../types/types";
 
 interface WeatherInformationProps {
   city: string;
-  data: object;
+  data: Data;
 }
 
 const Countries: { [key: string]: string; } = {
-  'Berlin': 'Berlin, DE',
-  'London': 'London, UK',
-  'Tokyo': 'Tokyo, JP',
+  'Berlin': 'BERLIN, DE',
+  'London': 'LONDON, UK',
+  'Tokyo': 'TOKYO, JP',
+}
+
+export const ICONS: { [key: string]: string } = {
+  'Sunny': '/icons/sunny.png',
+  'Partly cloudy': '/icons/partlyCloudy.png',
+  'Light rain shower': '/icons/lightRainyShower.png',
+  'Cloudy': '/icons/cloudy.png',
+  'Overcast': '/icons/overcast.png',
+  'Mist': '/icons/mist.png',
+  'Partly rain possible': '/icons/partlyRainPossible.png',
+  'Patchy snow possible': '/icons/partlySnowPossible.png',
+  'Patchy sleet possible': '/icons/partlySleetPossible.png',
+  'Patchy freezing drizzle possible': '/icons/patchyFreezingDrizzlePossible.png',
+  'Patchy light drizzle': '/icons/patchyFreezingDrizzlePossible.png',
+  'Light drizzle': '/icons/patchyFreezingDrizzlePossible.png',
+  'Freezing drizzle': '/icons/patchyFreezingDrizzlePossible.png',
+  'Heavy freezing drizzle': '/icons/patchyFreezingDrizzlePossible.png',
+  'Thundery outbreaks possible': '/icons/thunderyOutbreaksPossible.png',
+  'Blowing snow':'/icons/blowingSnow.png',
+  'Blizzard': '/icons/blizzard.png',
+  'Fog': '/icons/mist.png',
+  'Freezing fog': '/icons/mist.png',
+  'Patchy light rain': '/icons/lightRainyShower.png',
+  'Light rain': '/icons/lightRainyShower.png',
+  'Moderate rain at times': '/icons/lightRainyShower.png',
+  'Moderate rain': '/icons/lightRainyShower.png',
+  'Heavy rain at times': '/icons/lightRainyShower.png',
+  'Heavy rain': '/icons/lightRainyShower.png',
+  'Light freezing rain': '/icons/lightRainyShower.png',
+  'Moderate or heavy freezing rain': '/icons/lightRainyShower.png',
+  'Light sleet': '/icons/partlySleetPossible.png',
+  'Moderate or heavy sleet': '/icons/partlySleetPossible.png',
+  'Patchy light snow':'/icons/blowingSnow.png',
+  'Light snow':'/icons/blowingSnow.png',
+  'Patchy moderate snow':'/icons/blowingSnow.png',
+  'Moderate snow':'/icons/blowingSnow.png',
+  'Patchy heavy snow':'/icons/blowingSnow.png',
+  'Heavy snow':'/icons/blowingSnow.png',
+  'Ice Pellets':'/icons/icePellets.png',
+  'Moderate or heavy rain shower': '/icons/lightRainyShower.png',
+  'Torrential rain shower': '/icons/lightRainyShower.png',
+  'Light sleet showers': '/icons/partlySleetPossible.png',
+  'Moderate or heavy sleet showers': '/icons/partlySleetPossible.png',
+  'Light snow showers':'/icons/blowingSnow.png',
+  'Moderate or heavy snow showers':'/icons/blowingSnow.png',
+  'Light showers of ice pellets':'/icons/icePellets.png',
+  'Patchy rain possible': '/icons/partlyRainPossible.png',
+  'Moderate or heavy showers of ice pellets':'/icons/icePellets.png',
+  'Patchy light rain with thunder': '/icons/patchyLightRainWithThunder.png',
+  'Moderate or heavy rain with thunder': '/icons/patchyLightRainWithThunder.png',
+  'Patchy light snow with thunder': '/icons/patchyLightSnowWithThunder.png',
+  'Moderate or heavy snow with thunder': '/icons/patchyLightSnowWithThunder.png',
+  'Clear': '/icons/clear.png',
 }
 
 const WeatherInformation = ({data, city}: WeatherInformationProps) => {
   return(
     <div className={style.weatherInformation}>
       <div className={style.cityTemputureBox}>
-        <img src="" alt=""/>
-        <span>klfka</span>
+        <img className={style.icons} src={ICONS[data?.current?.condition.text]} alt="weather icon"/>
+        <span className={style.weatherTemperature}>{data?.current?.temp_c}&deg;</span>
       </div>
+      <div className={style.line}/>
       <div className={style.cityInformationBox}>
-        <span>dfjsdfk</span>
-        <p>{Countries[city as string]}</p>
+        <span className={style.weatherText}>{data?.current?.condition.text}</span>
+        <p className={style.cityText}>{Countries[city as string]}</p>
       </div>
     </div>
   )
